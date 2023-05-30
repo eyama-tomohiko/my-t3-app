c7052647ac71ada14bb3ee2891e050887750f19c
eyama-tomohiko <eymtmhk@gmail.comm>
2023-05-30 20:39:24 +0900

    Profile page with ssg helper

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,21 +1,13 @@
 import { z } from "zod";
-import type { User } from "@clerk/nextjs/dist/api";
 import { clerkClient } from "@clerk/nextjs/server";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 import { TRPCError } from "@trpc/server";
 import { privateProcedure } from "~/server/api/trpc";
 
-const filterForUserClient = (user: User) => {
-  return {
-    id: user.id,
-    username: user.username,
-    profileImageUrl: user.profileImageUrl,
-  };
-};
-
 import { Ratelimit } from "@upstash/ratelimit"; // for deno: see above
 import { Redis } from "@upstash/redis";
+import { filterForUserClient } from "~/server/helpers/filterUserForClient";
 
 // Create a new ratelimiter, that allows 10 requests per 1 minute
 const ratelimit = new Ratelimit({
